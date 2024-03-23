ab54eff999db7322ece98ebfa3e297599779069c
IkramAmh <ikramamhirrach2@gmail.com>
2024-03-23 10:12:40 -0700

    print_alphabt

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
-for i in [x for x in range(97, 123) if (x != 101) & (x != 113)]:
+for i in range(97, 123):
+    if (i == 101) | (i == 113):
+        continue
     print("{}".format(chr(i)), end="")
